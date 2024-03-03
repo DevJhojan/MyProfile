6e2b5ca99373c496279acffab43699adb6f2d1a1
DevJhojan <jhojan1806@gmail.com>
2024-03-02 20:34:30 -0500

    links no disponibles

--- src/app/home/software-developer/projects/projects.component.ts
+++ src/app/home/software-developer/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
-interface Project{
+interface Project {
   img: string;
   title: string;
   url: string;
+  enable: boolean;
 }
 
 @Component({
@@ -11,10 +12,13 @@ interface Project{
   standalone: false,
   template: `
     <div class="w-100 row wrap">
-      <article class="w-15 column" *ngFor="let project of projects">
-        <img class="hr-6" [src]="project.img" [alt]="project.title">
+      <article
+        class="w-15 column"
+        *ngFor="let project of projects"
+      >
+        <img class="hr-6" [src]="project.img" [alt]="project.title" />
         <h3>
-          <a [href]="project.url">{{project.title}}</a>
+          <a [href]="project.url" >{{ project.enable?project.title:"No Disponible" }}</a>
         </h3>
       </article>
     </div>
@@ -22,8 +26,23 @@ interface Project{
 })
 export class ProjectsComponent {
   projects: Project[] = [
-    {img: 'assets/img/pokedesk.png', title: 'Pokedesk', url:'https://pokedesk-lilac.vercel.app/'},
-    {img: 'assets/img/Dragons.png', title: 'Dragons', url:'https://github.com/DevJhojan/Dragon'},
-    {img: 'assets/img/WebTask.png', title: 'WebTasks', url:''}
+    {
+      img: 'assets/img/pokedesk.png',
+      title: 'Pokedesk',
+      url: 'https://pokedesk-lilac.vercel.app/',
+      enable: true,
+    },
+    {
+      img: 'assets/img/Dragons.png',
+      title: 'Dragons',
+      url: '#',
+      enable: false,
+    },
+    {
+      img: 'assets/img/WebTask.png',
+      title: 'WebTasks',
+      url: '#',
+      enable: false,
+    },
   ];
 }
